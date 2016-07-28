088bd3ce8e486fb1df70099a46d9e5fef6d1f535
Juan <Peaceall0@gmail.com>
2016-07-28 09:16:59 -0400

    initial push

---
+++ p2/global/header.php
@@ -0,0 +1,5 @@
+<h1>Project 2</h1>
+<h3>Basic server-side validation</h3>
+<p class='lead'>This project uses PHP server-side validation.</p>
+<p class='lead'> Adds edit/delete command functionality to A5 </p>
+(Table: petstore)
